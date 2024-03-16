e4bcb5d8050abc0c60e0270d67cb0a23ccc3162e
Sara Jendeberg <sara.jendeberg@gmail.com>
2024-03-16 14:14:29 +0100

    Add basic Springboot Application

---
+++ src/test/java/com/motherbrain/enhancer/EnhancerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.motherbrain.enhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
